dd52ffd4855265bbf3c06a66fc09de9aeffb66bd
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-05-04 14:51:20 +0100

    Query multiple tables

--- data.sql
+++ data.sql
@@ -14,3 +14,26 @@ INSERT INTO animals(Id, name, date_of_birth, weight_kg, neutered, escape_attempt
 			(9, 'Boarmon', '2005-6-7', 20.4, true, 7),
 			(10, 'Blossom', '1998-10-13', 17, true, 3),
 			(11, 'Ditto', '2022-5-14', 22, true, 4);
+
+INSERT INTO owners(full_name, age) 
+	VALUES ('Sam Smith', 34),
+			('Jennifer Orwell', 19),
+			('Bob', 45),
+			('Melody Pond', 77),
+			('Dean Winchester', 14),
+			('Jodie Whittaker', 38);
+
+UPDATE animals
+SET species_id = CASE
+WHEN name LIKE '%mon%' THEN (SELECT id FROM species WHERE name = 'Digimon')
+ELSE(SELECT id FROM species WHERE name = 'Pokemon')
+END;
+
+UPDATE animals
+SET owner_id = CASE
+WHEN name = 'Agumon' THEN(SELECT id FROM owners WHERE full_name = 'Sam Smith')
+WHEN name IN ('Gabumon', 'Pikachu') THEN(SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+WHEN name IN ('Devimon', 'Plantmon') THEN(SELECT id FROM owners WHERE full_name = 'Bob')
+WHEN name IN('Charmander', 'Squirtle','Blossom') THEN(SELECT id  FROM owners WHERE full_name = 'Melody Pond')
+WHEN name IN('Angemon', 'Boarmon') THEN(SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+END;
--- schema.sql
+++ schema.sql
@@ -10,3 +10,24 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE IF EXISTS  animals ADD species varchar(255);
+
+DROP TABLE IF EXISTS owners;
+CREATE TABLE owners (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  full_name VARCHAR(100),
+  age INTEGER
+);
+
+DROP TABLE IF EXISTS species;
+CREATE TABLE species (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(100)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals
+ADD species_id INT,
+ADD owner_id INT,
+ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
+ADD CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES owners(id);
